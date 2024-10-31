32f3ac18f064ebfa1eaf0bef60457a910c92960e
promhuadz <livingjoy@Livings-iMac.local>
2024-10-31 10:35:14 +0700

    Tset commimt

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  baseUrl: ".",
-  paths: {
-    "@/app/*": ["app/*"]
-  }
+
 };
 
 export default nextConfig;
